b284e9f6e439c36bb040deb0a63e150a2ad8a45e
Adedamola Ajasa <azeemajasa@gmail.com>
2023-08-16 08:50:13 +0100

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,5 +1,18 @@
 const init = () => {
-  
+    const form = document.querySelector("#form")
+    form.addEventListener("submit", function(e){
+        e.preventDefault();
+        const input = document.querySelector("input#searchByID ")
+        fetch(`http://localhost:5500/movies/${input.value}`)
+        .then((response)=> response.json())
+        .then((data)=> {
+            const title = document.querySelector("section#movieDetails h4")
+            const summary = document.querySelector("section#movieDetails p")
+            title.textContent = data.title
+            summary.textContent = data.summary
+        })
+
+    })
 }
 
-document.addEventListener('DOMContentLoaded', init);
+document.addEventListener('DOMContentLoaded', init)
